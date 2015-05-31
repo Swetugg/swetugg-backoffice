155c23aac38ef44779ff9640a5f75b1bd8c83c5f
Anders Ljusberg <andlju@gmail.com>
2015-05-31 19:56:29 +0200

    Support manual migrations

--- src/Swetugg.Web/Controllers/HomeController.cs
+++ src/Swetugg.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Swetugg.Web.Migrations;
 
 namespace Swetugg.Web.Controllers
 {
@@ -18,5 +20,40 @@ namespace Swetugg.Web.Controllers
         {
             return View();
         }
+
+	    [Route("migration")]
+        [RequireHttps]
+        [Authorize(Roles = "Administrator")]
+	    public ActionResult Migrations(string errorMsg)
+	    {
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+	        ViewBag.AppliedMigrations = migrator.GetDatabaseMigrations();
+	        ViewBag.PendingMigrations = migrator.GetPendingMigrations();
+	        ViewBag.ErrorMessage = errorMsg;
+
+	        return View();
+	    }
+
+        [Route("migration/run")]
+        [RequireHttps]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult RunMigrations()
+        {
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+
+            try
+            {
+                migrator.Update();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Migrations", new { errorMsg = ex.Message});
+            }
+
+            return RedirectToAction("Migrations");
+        }
+
 	}
 }
