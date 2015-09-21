0722ea8aa00f0fb5ff54018ecc8fbb3350dfbc77
Anders Ljusberg <andlju@gmail.com>
2015-09-21 22:02:05 +0200

    Some CFP fixes

---
+++ src/Swetugg.Web/Helpers/WebSiteLinkHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace Swetugg.Web.Helpers
+{
+    public static class WebSiteLinkHelpers
+    {
+        public static string EnsureHttpLink(this string link)
+        {
+            if (string.IsNullOrWhiteSpace(link))
+                return link;
+
+            var trimmed = link.Trim();
+            if (trimmed.StartsWith("https://") || trimmed.StartsWith("http://"))
+                return trimmed;
+            if (trimmed.Contains("."))
+                return "http://" + trimmed;
+
+            return link;
+        }
+    }
+}
