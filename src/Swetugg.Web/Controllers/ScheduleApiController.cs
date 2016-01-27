4fdd2999aeff5168361dc0ad4a01a34a4e5fc642
Anders Ljusberg <andlju@gmail.com>
2016-01-27 21:19:20 +0100

    Adding filtering and more info to the schedule api

--- src/Swetugg.Web/Controllers/ScheduleApiController.cs
+++ src/Swetugg.Web/Controllers/ScheduleApiController.cs
@@ -9,10 +9,20 @@ using Swetugg.Web.Models;
 
 namespace Swetugg.Web.Controllers
 {
+    public class SpeakerView
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Bio { get; set; }
+        public string Twitter { get; set; }
+        public string Web { get; set; }
+        public string GitHub { get; set; }
+    }
+
     public class SessionView
     {
         public string Name { get; set; }
-        public string[] Speakers { get; set; }
+        public SpeakerView[] Speakers { get; set; }
         public string Room { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
@@ -35,18 +45,35 @@ namespace Swetugg.Web.Controllers
         public async Task<ActionResult> Index()
         {
             var sessions = await GetSessions();
-
+            var roomSlug = Request.QueryString["room"];
+            var dateParam = Request.QueryString["date"];
+            DateTime date = DateTime.MinValue;
+            if (dateParam != null)
+            {
+                DateTime.TryParse(dateParam, out date);
+            }
             var cultureInfo = CultureInfo.GetCultureInfo("sv-SE");
 
             var sessionsView = from s in sessions
                 where s.RoomSlots.Any()
                 let roomSlot = s.RoomSlots.First()
+                               where 
+                                (roomSlug == null || roomSlot.Room.Slug == roomSlug) &&
+                                (date == DateTime.MinValue || roomSlot.Slot.Start.Date == date)
                                orderby roomSlot.Slot.Start, roomSlot.Room.Name
                 select new SessionView()
                 {
                     Name = s.Name,
                     Description = s.Description,
-                    Speakers = s.Speakers.Select(sp => sp.Speaker.Name).ToArray(),
+                    Speakers = s.Speakers.Select(sp => new SpeakerView()
+                    {
+                        Name = sp.Speaker.Name, 
+                        Company = sp.Speaker.Company,
+                        Bio = sp.Speaker.Bio,
+                        Twitter = sp.Speaker.Twitter,
+                        Web = sp.Speaker.Web,
+                        GitHub = sp.Speaker.GitHub
+                    }).ToArray(),
                     StartTime = roomSlot.Slot.Start.ToString(cultureInfo),
                     EndTime = roomSlot.Slot.End.ToString(cultureInfo),
                     Room = roomSlot.Room.Name
