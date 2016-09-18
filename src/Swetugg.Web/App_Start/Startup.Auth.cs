b2d6b965d18e73155bd47ea09196216da72e171b
Anders Ljusberg <andlju@gmail.com>
2016-09-18 19:51:30 +0200

    Updated twitter certs

--- src/Swetugg.Web/App_Start/Startup.Auth.cs
+++ src/Swetugg.Web/App_Start/Startup.Auth.cs
@@ -64,9 +64,20 @@ namespace Swetugg.Web
             }
             if (IsApiEnabled("Twitter"))
             {
-                app.UseTwitterAuthentication(
-                   consumerKey: ConfigurationManager.AppSettings["Twitter_Api_ConsumerKey"],
-                   consumerSecret: ConfigurationManager.AppSettings["Twitter_Api_ConsumerSecret"]);
+                app.UseTwitterAuthentication(new Microsoft.Owin.Security.Twitter.TwitterAuthenticationOptions
+                {
+                    ConsumerKey = ConfigurationManager.AppSettings["Twitter_Api_ConsumerKey"],
+                    ConsumerSecret = ConfigurationManager.AppSettings["Twitter_Api_ConsumerSecret"],
+                    BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+                    {
+                        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                        "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                        "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                    })
+                });
             }
 
             if (IsApiEnabled("Facebook"))
