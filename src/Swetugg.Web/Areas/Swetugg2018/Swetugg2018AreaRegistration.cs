bd1a00b30a075cf72e5234952c5bfcd75ddf11c1
Anders Ljusberg <andlju@gmail.com>
2017-08-22 20:22:15 +0200

    Swetugg2018 first checkin

---
+++ src/Swetugg.Web/Areas/Swetugg2018/Swetugg2018AreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Swetugg.Web.Areas.Swetugg2018
+{
+    public class Swetugg2018AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "swetugg-2018";
+            }
+        }
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+        }
+    }
+}
