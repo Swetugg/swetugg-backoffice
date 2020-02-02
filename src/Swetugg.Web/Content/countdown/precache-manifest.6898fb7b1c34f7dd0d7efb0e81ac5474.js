e51b3edddd7215585b12451c92f5c86c097e53c3
Anders Ljusberg <andlju@gmail.com>
2020-02-02 12:02:59 +0100

    Added countdown timer

---
+++ src/Swetugg.Web/Content/countdown/precache-manifest.6898fb7b1c34f7dd0d7efb0e81ac5474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4b4f1f24baae5be4b176608613a695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d4b75e4041fb6d8c6a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a7cacee4ca8a0bbe33bc",
+    "url": "/static/css/main.b1ee6ce9.chunk.css"
+  },
+  {
+    "revision": "82d4b75e4041fb6d8c6a",
+    "url": "/static/js/2.f15cb981.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f15cb981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7cacee4ca8a0bbe33bc",
+    "url": "/static/js/main.16aade0b.chunk.js"
+  },
+  {
+    "revision": "704d9fc942ca383021d2",
+    "url": "/static/js/runtime-main.09a5ef67.js"
+  }
+]);
