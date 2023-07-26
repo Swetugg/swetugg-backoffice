e16ef0b81a8d204f0155ac4cbd26fd16521e83af
Cecilia Wirén <cecilia.wiren@activesolution.se>
2023-07-26 15:39:36 +0200

    gbg23 area added

---
+++ src/Swetugg.Web/Areas/Gbg2023/Gbg2023AreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Swetugg.Web.Areas.Gbg2022
+{
+    public class Gbg2023AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "gbg-2023";
+            }
+        }
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+        }
+    }
+}
